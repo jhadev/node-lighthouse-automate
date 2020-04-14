fd59c815acf8c5f73a0d04c3c9cd748add31badc
jhadev <joshappeldev@gmail.com>
2020-04-14 14:44:34 -0400

    save to mongo works, have to fix token

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+const fs = require('fs');
+const { promisify } = require('util');
 const sh = require('shelljs');
 
-const command = `lighthouse https://development-global-coach.demandware.net/on/demandware.store/Sites-Coach_US-Site --output json --output-path ./reports/report-${Date.now()}.json --chrome-flags="--headless" --save-assets`;
+const read = promisify(fs.readFile);
+const write = promisify(fs.writeFile);
 
-sh.exec(command, (code, output) => {
-  console.log(output);
-  // sh.echo(`exit code ${code}`);
-});
+let now = Date.now();
+
+const command = `lighthouse https://development-global-coach.demandware.net/on/demandware.store/Sites-Coach_US-Site --output json --output-path ./reports/report-${now}.json --chrome-flags="--headless" --save-assets`;
+
+const importIntoMongo = `mongoimport --jsonArray -d lighthouse-automation -c data --file .//reports/currentReport.json`;
+
+const runLighthouse = () => {
+  return new Promise((resolve, reject) => {
+    sh.exec(command, (code, output) => {
+      console.log(output);
+      resolve();
+    });
+  });
+};
+
+const saveToMongo = async () => {
+  await runLighthouse();
+  const path = `./reports/report-${now}.json`;
+  let data = await read(path, 'utf8');
+  data = [JSON.parse(data)];
+
+  await write('./reports/currentReport.json', JSON.stringify(data));
+  sh.exec(importIntoMongo, (code, output) => {
+    console.log(output);
+  });
+};
+
+saveToMongo();
