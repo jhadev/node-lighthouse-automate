ab737243a58e03c2f3362bbd18ba5badd868e05d
jhadev <joshappeldev@gmail.com>
2020-04-14 15:18:57 -0400

    add .env

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const fs = require('fs');
 const { promisify } = require('util');
 const sh = require('shelljs');
+require('dotenv').config();
 
 const read = promisify(fs.readFile);
 const write = promisify(fs.writeFile);
 
 let now = Date.now();
 
-const command = `lighthouse https://development-global-coach.demandware.net/on/demandware.store/Sites-Coach_US-Site --output json --output-path ./reports/report-${now}.json --chrome-flags="--headless" --save-assets`;
+const command = `lighthouse ${process.env.URL} --output json --output-path ./reports/report-${now}.json --chrome-flags="--headless" --save-assets`;
 
-const importIntoMongo = `mongoimport --jsonArray -d lighthouse-automation -c data --file .//reports/currentReport.json`;
+const importIntoMongo = `mongoimport --jsonArray -d lighthouse-automation -c data --file .//reports/lastSavedReport.json`;
 
 const runLighthouse = () => {
   return new Promise((resolve, reject) => {
