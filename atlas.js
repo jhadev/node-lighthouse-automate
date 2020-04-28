f7907e7b0da530d7df7e07868769ac66f0a80b77
jhadev <joshappeldev@gmail.com>
2020-04-28 01:06:05 -0400

    console logs for confirmation

--- atlas.js
+++ atlas.js
@@ -7,6 +7,9 @@ const yesterday = moment().subtract(1, 'days').format('YYYY-MM-DD');
 const sendToAtlas = (date) => {
   const importToDb = `mongoimport --uri "${process.env.MONGO_CONNECT}" --collection averages --file .//reports/dailyAverage/${date}-score.json`;
   sh.exec(importToDb, (code, output) => {
+    console.log(
+      `Lighthouse data from ${yesterday} for ${process.env.URL} sent to ${process.env.MONGO_CONNECT}`
+    );
     console.log(output);
   });
 };
